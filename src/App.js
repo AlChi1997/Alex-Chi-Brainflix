d4d6c297b19531cd7d51273fc21500b5d52edf2e
AlChi1997 <alex.chi1997@hotmail.com>
2023-08-06 16:35:20 -0400

    Created app.scss file for some global styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Upload from "./pages/Upload/Upload";
+import "./styles/app.scss";
 
 function App() {
   return (
