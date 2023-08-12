6ac48c43d53af81628a2d20a3bdecab6ee845b0b
AlChi1997 <alex.chi1997@hotmail.com>
2023-08-12 17:50:44 -0400

    had to install express and node modules

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || process.argv[2] || 8080;
+const { v4: uuidv4 } = require('uuid');
+require ('dotenv').config();
+const videos = require('./videolist-data/videos');
+const accomodation = require('./videos/accommodation');
+const cruising = require('./videos/cruising');
+const airline = require('./videos/airline');
+const BMX = require('./videos/BMX');
+const boutique = require('./videos/boutique');
+const hiddengem = require('./videos/hiddengem');
+const train = require('./videos/train');
+const travelhealth = require('./videos/travelhealth');
+const travelpro = require('./videos/travelpro');
+
+app.use(express.json());
+
+app.get('/videos', (req, res) => {
+    //send videos array as JSON response
+    res.json(videos);
+});
 
 app.listen(port, () => {
-    console.log('Listening on ${port}');
+    console.log("listening on port 8080");
 });
